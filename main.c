8e026cc3aaca19eaca9b88a09e2083f7994b1807
compyx <b.wassink@ziggo.nl>
2023-08-24 23:23:03 +0200

    Add better testing of expression evaluation
    Update file parser in test program to scan for expected error number,
    expected evaluation result (if error number is 0) and the expression to
    test.

--- main.c
+++ main.c
@@ -4,6 +4,7 @@
 #include <libgen.h>
 #include <errno.h>
 #include <ctype.h>
+#include <limits.h>
 
 #include "boolexpr.h"
 
@@ -12,25 +13,35 @@
  */
 static char line[256];
 
+static char *prgname;
+
 
 /** \brief  Print usage message on stdout
  *
  * \param[in]   argv0   contents of \c argv[0], the path to the executable
  */
-static void usage(char *argv0)
+static void usage(void)
 {
-    printf("Usage: %s <filename>\n", basename(argv0));
+    printf("Usage: %s <filename>\n", prgname);
 }
 
 /** \brief  Print I/O error number and message on stderr
  *
  * \param[in]   funcname    function name
  */
-static void print_ioerror(const char *funcname)
+static void print_ioerror(void)
 {
     fprintf(stderr,
-            "%s(): I/O error %d (%s).\n",
-            funcname, errno, strerror(errno));
+            "%s: I/O error %d (%s).\n",
+            prgname, errno, strerror(errno));
+}
+
+static char *skip_whitespace(char *s)
+{
+    while (*s != '\0' && isspace((unsigned char)*s)) {
+        s++;
+    }
+    return s;
 }
 
 /** \brief  Parse file \a path to test boolean expression handling
@@ -48,47 +59,100 @@ static bool parse_file(const char *path)
 
     fp = fopen(path, "rb");
     if (fp == NULL) {
-        print_ioerror(__func__);
+        print_ioerror();
         return false;
     }
 
     bexpr_init();
 
     while (true) {
+        char *curpos;
+        char *endptr;
+        long  errnum_exp = 0;   /* expected error number */
+        bool  result_exp = false;   /* expected result of evaluation */
+
         bexpr_reset();
 
         memset(line, 0, sizeof line);
         if (fgets(line, (int)sizeof line, fp) == NULL) {
             if (!feof(fp)) {
-                print_ioerror(__func__);
+                print_ioerror();
                 status = false;
             }
             break;
         }
+
         /* trim trailing whitespace, including newline/carriage return */
         for (int i = (int)strlen(line) - 1; i >= 0 && isspace((unsigned char)(line[i])); i--) {
             line[i] = '\0';
         }
-        printf("[%2d] Tokenizing \"%s\"\n", lineno, line);
 
-        if (!bexpr_tokenize(line)) {
+        /* skip leading whitespace */
+        curpos = skip_whitespace(line);
+        if (*curpos == '#' || *curpos == '\0') {
+            /* comment or empty line: skip line */
+            lineno++;
+            continue;
+        }
+
+        /* get expected error number */
+        errno = 0;
+        errnum_exp = strtol(curpos, &endptr, 10);
+        if ((errno == ERANGE) || curpos == endptr) {
+            fprintf(stderr,
+                    "%s:%d:%d: Expected error number.\n",
+                    path, lineno, (int)(line - curpos));
+            status = false;
+            goto cleanup;
+        }
+        curpos = endptr;
+
+        /* get expected result if expected result is 0 */
+        if (errnum_exp == 0) {
+            curpos = skip_whitespace(curpos);
+            if (strncmp(curpos, "true", 4u) == 0) {
+                curpos += 4;
+                result_exp = true;
+            } else if (strncmp(curpos, "false", 5u) == 0) {
+                curpos += 5;
+                result_exp = false;
+            } else {
+                fprintf(stderr,
+                        "%s:%d:%d: Expected evaluation result (\"true\" or"
+                        " \"false\").\n",
+                        path, lineno, (int)(line - curpos));
+                goto cleanup;
+            }
+        }
+
+        curpos = skip_whitespace(curpos);
+        printf("[%2d] Tokenizing \"%s\"\n", lineno, curpos);
+
+        if (!bexpr_tokenize(curpos)) {
             printf("[%2d] Parse error.\n", lineno);
         } else {
             bool result = false;
+
             printf("[%2d] OK: ", lineno);
             bexpr_print();
 
             printf("[%2d] Evaluating:\n", lineno);
             if (bexpr_evaluate(&result)) {
-                printf("[%2d] OK: %s\n", lineno, result ? "true" : "false");
+                printf("[%2d] ok: %s\n", lineno, result ? "true" : "false");
             } else {
-                printf("[%2d] FAIL.\n", lineno);
+                printf("[%2d] fail.\n", lineno);
             }
+            if ((result == result_exp) && (bexpr_errno == errnum_exp)) {
+                printf("TEST PASS\n");
+            } else {
+                printf("TEST FAIL\n");
+            }
+
         }
         putchar('\n');
         lineno++;
     }
-
+cleanup:
     bexpr_free();
     fclose(fp);
     return status;
@@ -105,13 +169,16 @@ static bool parse_file(const char *path)
 int main(int argc, char *argv[])
 {
     if (argc < 2) {
-        usage(argv[0]);
+        usage();
         return EXIT_FAILURE;
     }
     if (strcmp(argv[1], "--help") == 0) {
-        usage(argv[0]);
+        usage();
         return EXIT_SUCCESS;
     }
+
+    /* generate program name for messages */
+    prgname = basename(argv[0]);
 
     printf("Parsing '%s':\n", argv[1]);
     if (parse_file(argv[1])) {
